1097756851e39f0aa34b6b98ac42112b4b85c93f
jasminegmp <jasminekgump@gmail.com>
2019-07-12 21:45:08 -0700

    Route between home, sign in and sign up pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from '../NavBar';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
+import HomePage from '../Home';
 
 import * as ROUTES from '../../constants/routes';
 
@@ -10,7 +11,9 @@ const App = () => {
     return(
         <Router>
             <NavBar/>
-            <SignInPage/>
+            <Route path= {ROUTES.SIGN_UP} component = {SignUpPage} />
+            <Route path= {ROUTES.SIGN_IN} component = {SignInPage} />
+            <Route path= {ROUTES.HOME} component = {HomePage} />
         </Router>
     )
 };
