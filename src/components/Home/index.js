38ac9ef8acb3870498d65f7e33f35bcda344b4f5
jasminegmp <jasminekgump@gmail.com>
2019-08-11 00:33:33 -0700

    Home page update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,10 @@ const HomePage = () => {
     return (
         <div>
             <h1>Home</h1>
-             <p>The Home Page is accessible by every signed in user!</p>
+            <p>Name Origin: Eudora is a girl's name of Greek origin which means "good gift".</p>
+            <p>The goal of Eudora is for users to create a wishlist of personalized gifts. </p>
+            <p></p>
+            <p>Features coming in the future: Add items outside of Etsy, Group Gifting Exchanges like White Elephant, Child Wishlist for Your Children, and more!</p>
         </div>
     );
 };
